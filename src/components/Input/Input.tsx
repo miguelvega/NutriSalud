970fe57e302c6436b7f131d4f807fb9b73bddb59
Aushzlten12 <josepachas1604@gmail.com>
2024-11-06 00:51:29 -0500

    ft: Diseño del Login

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,22 @@
+import "./Input.css";
+
 interface Props {
   label: string;
   type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ label, type }: Props) => {
+export const Input = ({ label, type, value, onChange }: Props) => {
   return (
     <div className="box-input">
       <h3 className="label-input">{label}</h3>
-      <input type={type} className="input-base" />
+      <input
+        type={type}
+        className="input-base"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
