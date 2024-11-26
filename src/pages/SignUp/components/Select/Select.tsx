a8b378b3436f64945c0601fafa7e5028df590ac1
Aushzlten12 <josepachas1604@gmail.com>
2024-11-26 01:10:18 -0500

    Actualizando el formulario de registro añadiendo peso, talla y experiencia en caso sea nutricionista

--- src/pages/SignUp/components/Select/Select.tsx
+++ src/pages/SignUp/components/Select/Select.tsx
@@ -1,24 +1,35 @@
 import { FieldError } from "react-hook-form";
 import "./Select.css";
 
-interface Props {
-  register: any;
-  error?: FieldError;
+interface Option {
+  value: string;
+  label: string;
 }
 
-const Select = ({ register, error }: Props) => {
+interface Props {
+  register: any;
+  options: Option[];
+  error?: FieldError;
+  id?: string; // Opcional para personalizar el id
+  name: string; // Nombre del campo que se pasa al register
+}
+
+const Select = ({ register, options, error, id = "select", name }: Props) => {
   return (
     <div className="form-group">
-      <label className="label-signup" htmlFor="role">
+      <label className="label-signup" htmlFor={id}>
         Rol
       </label>
       <select
-        id="role"
-        {...register("role")}
+        id={id}
+        {...register(name)}
         className={`form-control ${error ? "is-invalid" : ""}`}
       >
-        <option value="paciente">Paciente</option>
-        <option value="nutricionista">Nutricionista</option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
       </select>
       {error && <span className="invalid-feedback">{error.message}</span>}
     </div>
